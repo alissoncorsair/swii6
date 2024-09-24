ac7abb22276b151473d60b37d14d9e6f9caf0d08
alisson <alissoncorsair@hotmail.com>
2024-09-24 14:52:36 -0300

    tp 01

---
+++ TP01/Controller/BookController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP01.Interface;
+using TP01.Models;
+using TP01.Repository;
+
+// Alisson de Sousa Vieira
+// Leonardo de Fontes
+
+namespace TP01.Controller
+{
+    public class BookController
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookController() {
+            this._bookRepository = new BookRepositoryCsv();
+            this.cargaInicial();
+        }
+
+        private void cargaInicial()
+        {
+            var books = this._bookRepository.getAll();
+            if(books.Count <= 0)
+            {
+                Author[] authors = {
+                    new Author("Stephen King", "stephenking@example.com", 'm'),
+                    new Author("Agatha Christie", "agathachristie@example.com", 'f')
+                };
+
+                Book book = new Book("The Shining", authors, 19.99, 250);
+
+                this._bookRepository.add(book);
+            }
+        }
+
+        public void DesmostrarMetodos()
+        {
+            Author author1 = new Author("Neil Gaiman", "neilgaiman@example.com", 'm');
+            Author author2 = new Author("Margaret Atwood", "matwood@example.com", 'f');
+
+            Author[] authors = { author1, author2 };
+
+            Book book = new Book("Good Omens", authors, 19.99, 75);
+
+            Console.WriteLine("Nome do livro: " + book.getName());
+            Console.WriteLine("Autores: " + book.getAuthorNames());
+            Console.WriteLine("Preço do livro: " + book.getPrice());
+            Console.WriteLine("Quantidade disponível: " + book.getQty());
+
+            book.setPrice(17.99);
+            book.setQty(40);
+
+            Console.WriteLine("Novo preço do livro: " + book.getPrice());
+            Console.WriteLine("Nova quantidade disponível: " + book.getQty());
+
+            Console.WriteLine(book.ToString());
+        }
+
+        public Task GetNameBook(HttpContext context) {
+            var book = _bookRepository.getAll().FirstOrDefault();
+
+            return context.Response.WriteAsync(book.getName());
+        }
+        public Task GetBook(HttpContext context) {
+            var book = _bookRepository.getAll().FirstOrDefault();
+
+            return context.Response.WriteAsync(book.ToString());
+        }
+        public Task GetAuthorsBook(HttpContext context) {
+            var book = _bookRepository.getAll().FirstOrDefault();
+
+            return context.Response.WriteAsync(book.getAuthorNames());
+        }
+        public Task GetHtmlBook(HttpContext context) {
+            var book = _bookRepository.getAll().FirstOrDefault();
+
+            var authors = book.getAuthors().Select(a => $"<li>{a.Name}</li>");
+
+            return context.Response.WriteAsync(@$"
+                <h1>{book.getName()}</h1>    
+
+                <strong>Autores:</strong>
+                <ol>
+                    {string.Join("", authors)}
+                </ol>
+            ");
+        }
+    }
+}
---
+++ TP01/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
+using TP01;
+using TP01.Controller;
+
+// Alisson de Sousa Vieira
+// Leonardo de Fontes
+
+var controller = new BookController();
+
+controller.DesmostrarMetodos();
+
+Console.WriteLine("\n\n\n\n");
+
+IWebHost host = new WebHostBuilder()
+    .UseKestrel()
+    .UseStartup<Startup>()
+    .Build();
+
+host.Run();
